29787f954005997c035bc57c1f2a4c868ad3dac6
eoqna <eoqna@naver.com>
2023-11-07 09:37:52 +0900

    Promise, Awaited, bind 학습

--- first.ts
+++ first.ts
@@ -521,3 +521,62 @@ type Params = PR<typeof zip>;
 type First = Params[2];
 // type Ret = ReturnType<typeof zip>;
 type Ret = RT<typeof zip>;
+
+// 클래스는 type으로 바로 쓸 수 있다.
+// 정확히는 instance(new) 이다.
+class CCP {
+  a: string;
+  b: number;
+  c: boolean;
+  constructor(a: string, b: number, c: boolean) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+};
+
+const CCCP = new CCP("123", 456, true);
+// 매개 변수의 타입을 가져온다.
+type CPA = ConstructorParameters<typeof CCP>;
+
+// 생성자의 타입을 가져온다.
+type ITA = InstanceType<typeof CCP>;
+
+// Promise, Awaited
+// Promise
+const p1 = Promise.resolve(1).then((a) => a + 1).then((a) => a + 1).then((a) => a.toString());
+const p2 = Promise.resolve(2);
+const p3 = new Promise((res, rej) => {
+  setTimeout(res, 1000);
+});
+
+Promise.all([p1, p2, p3]).then((result) => {
+  console.log(result); // ["3", 2, undefined]
+});
+
+// T = [p1, p2, p3] "0": p1, "1": p2, "2": p3, "length": 3
+// keyof T = 0 | 1 | 2 | length
+
+const pArr = [1, 2, 3] as const;
+type PArrT = keyof typeof pArr;
+const pkey: PArrT = 1;
+
+// TypeScript에서는 Promise대신 then이라는 객체를 사용해서 코드를 작성할 수 있다.
+type PromiseResult = Awaited<{then(onfullfilled: (v: number) => number): any}>; //thenable
+
+/** 
+ * bind : 
+*/
+
+function bindA(this: Window | typeof bindObjA) {
+  console.log(this);
+};
+
+const bindObjA = { name: "dbshin" };
+const bindObjB = bindA.bind(bindObjA);
+bindObjB();
+
+type TPT = ThisParameterType<typeof bindA>;
+
+
+// OmitThisParamter : type 추론 실패 시 그 type 그대로 사용하고, 성공 시 매개변수와 리턴값 그대로 만들어라.
